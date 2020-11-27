848641415ecd187b8b294c779d99088f76d2c0b2
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-27 09:57:41 +0100

    first components added

---
+++ src/components/ArtistPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ArtistPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
